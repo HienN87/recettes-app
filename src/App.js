6991b95ceab372f01424fec206da09dd4889173c
Hien <trong.hien.ngo@gmail.com>
2021-04-27 15:19:52 +0200

    ajout de 'hHader.js' et update du projet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 // CSS
 import './App.css'
+import Header from './components/Header'
 
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo
   }
 
-  render () {
+  render() {
     return (
       <div className='box'>
-        <h1>Bonjour {this.state.pseudo}</h1>
+        <Header pseudo={this.state.pseudo} />
+
         <div className='cards'>
           <div className='card'>
             <h2>Une Carte</h2>
