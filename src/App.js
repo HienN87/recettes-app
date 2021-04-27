5e8e6c99a674bf0eb6be1e86e0e17e6460f3039e
Hien <trong.hien.ngo@gmail.com>
2021-04-27 17:56:39 +0200

    ajout du component Card.js et traitement de l'affichage des cartes de recettes (images, formatage liste)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import './App.css'
 import Admin from './components/Admin'
 import Header from './components/Header'
+import Card from './components/Card'
 import recettes from './recettes'
 
 class App extends Component {
@@ -16,15 +17,14 @@ class App extends Component {
   }
 
   render() {
+    const cards = Object.keys(this.state.recettes)
+      .map(key => <Card key={key} details={this.state.recettes[key]} />)
+
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo} />
 
-        <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
-        </div>
+        {cards}
 
         <Admin chargerExemple={this.chargerExemple} />
       </div>
