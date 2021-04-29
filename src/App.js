fd75e6674e11653c8d07432e883bde1e2612a8f3
Hien <trong.hien.ngo@gmail.com>
2021-04-29 17:03:10 +0200

    ajout gestion maj recettes

--- src/App.js
+++ src/App.js
@@ -31,6 +31,12 @@ class App extends Component {
     this.setState({ recettes })
   }
 
+  majRecette = (key, newRecette) => {
+    const recettes = { ...this.state.recettes }
+    recettes[key] = newRecette
+    this.setState({ recettes })
+  }
+
   chargerExemple = () => {
     this.setState({ recettes })
   }
@@ -46,7 +52,9 @@ class App extends Component {
         {cards}
 
         <Admin
+          recettes={this.state.recettes}
           ajouterRecette={this.ajouterRecette}
+          majRecette={this.majRecette}
           chargerExemple={this.chargerExemple} />
       </div>
     )
