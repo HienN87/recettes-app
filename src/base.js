39b5adde539e70a1e4760da83b80aca537767e3d
Hien <trong.hien.ngo@gmail.com>
2021-04-30 22:13:30 +0200

    gestion maintenir connexion entre les sessions

--- src/base.js
+++ src/base.js
@@ -5,8 +5,7 @@ import 'firebase/database'
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBu2Cf1xYsMmlFKLA815-lLKOkDIiz0JO0",
   authDomain: "recettes-app-5ace7.firebaseapp.com",
-  databaseURL: "https://recettes-app-5ace7-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "recettes-app-5ace7"
+  databaseURL: "https://recettes-app-5ace7-default-rtdb.europe-west1.firebasedatabase.app"
 })
 
 const base = Rebase.createClass(firebaseApp.database())
