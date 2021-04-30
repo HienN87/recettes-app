65041fe7f4abfeda383ab5c07c084ea68c2a5f7e
Hien <trong.hien.ngo@gmail.com>
2021-04-30 19:08:27 +0200

    fonction deconnexion

--- src/base.js
+++ src/base.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBu2Cf1xYsMmlFKLA815-lLKOkDIiz0JO0",
   authDomain: "recettes-app-5ace7.firebaseapp.com",
   databaseURL: "https://recettes-app-5ace7-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "recettes-app-5ace7"
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -37,8 +37,15 @@ class Admin extends Component {
       .then(this.handleAuth)
   }
 
+  logout = async () => {
+    await firebase.auth().signOut()
+    this.setState({ uid: null })
+  }
+
   render() {
     const { recettes, ajouterRecette, majRecette, chargerExemple, supprimerRecette } = this.props
+
+    const logout = <button onClick={this.logout}>Déconnexion</button>
 
     // Si l'utilisateur n'est pas connecté
     if (!this.state.uid) {
@@ -49,6 +56,7 @@ class Admin extends Component {
       return (
         <div>
           <p>Vous n'êtes pas le chef de cette boîte !</p>
+          {logout}
         </div>
       )
     }
@@ -69,6 +77,7 @@ class Admin extends Component {
         }
 
         <footer>
+          {logout}
           <button onClick={chargerExemple}>Remplir</button>
         </footer>
       </div>
